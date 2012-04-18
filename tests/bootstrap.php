4839919b21400583cfbb5665570751a35e256da5
Geoff Adams <geoff@dianode.net>
2012-04-18 02:06:26 +0100

    Added test suite bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+require_once __DIR__ . '/../vendor/doctrine2/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
+
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', __DIR__ . '/../vendor/doctrine2/lib/vendor/doctrine-common/lib');
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', __DIR__ . '/../vendor/doctrine2/lib/vendor/doctrine-dbal/lib');
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\ORM', __DIR__ . '/../vendor/doctrine2/lib');
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('BedREST\Tests', __DIR__);
+$classLoader->register();
+
+$classLoader = new \Doctrine\Common\ClassLoader('BedREST', __DIR__ . '/../library');
+$classLoader->register();
