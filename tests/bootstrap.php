586f1554906bccf5c5d24a64c33087fdf47c88f0
Geoff Adams <geoff@dianode.net>
2012-07-01 00:53:51 +0100

    Implemented Composer support for dependency management.
    Removed usage of Git submodules and associated Phing target for updating them.
    Changes to test bootstrap to reflect change to vendor/ directory structure.

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -3,18 +3,18 @@
 // set global constants
 if (defined('BEDREST_LIB_PATH') || define('BEDREST_LIB_PATH', realpath(__DIR__ . '/../library/')));
 if (defined('BEDREST_TESTS_PATH') || define('BEDREST_TESTS_PATH', realpath(__DIR__ . '/../')));
-if (defined('DOCTRINE_LIB_PATH') || define('DOCTRINE_LIB_PATH', realpath(__DIR__ . '/../vendor/doctrine2/lib/')));
+if (defined('DOCTRINE_LIB_PATH') || define('DOCTRINE_LIB_PATH', realpath(__DIR__ . '/../vendor/doctrine/')));
 
 // initiate Doctrine class loader
 require_once __DIR__ . '/../vendor/doctrine2/lib/vendor/doctrine-common/lib/Doctrine/Common/ClassLoader.php';
 
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', DOCTRINE_LIB_PATH . '/vendor/doctrine-common/lib');
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\Common', DOCTRINE_LIB_PATH . '/common/lib');
 $classLoader->register();
 
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', DOCTRINE_LIB_PATH . '/vendor/doctrine-dbal/lib');
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\DBAL', DOCTRINE_LIB_PATH . '/dbal/lib');
 $classLoader->register();
 
-$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\ORM', DOCTRINE_LIB_PATH);
+$classLoader = new \Doctrine\Common\ClassLoader('Doctrine\ORM', DOCTRINE_LIB_PATH . '/orm/lib');
 $classLoader->register();
 
 $classLoader = new \Doctrine\Common\ClassLoader('BedRest\TestFixtures', __DIR__);
