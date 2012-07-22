eafe513d8e3c048afb86e325445c282c3d6049da
Geoff Adams <geoff@dianode.net>
2012-07-22 17:53:59 +0100

    Added base RestManager and ServiceManager classes.

---
+++ library/BedRest/RestManager.php
@@ -0,0 +1,153 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace BedRest;
+
+use BedRest\Configuration;
+use BedRest\Request;
+use BedRest\Response;
+use BedRest\Mapping\ResourceMetadataFactory;
+use Doctrine\Common\EventManager;
+
+/**
+ * RestManager
+ * 
+ * Responsible for dispatching REST actions to the correct services. Sits between controllers and the service layer.
+ *
+ * @author Geoff Adams <geoff@dianode.net>
+ */
+class RestManager
+{
+    /**
+     * Configuration instance.
+     * @var BedRest\Configuration 
+     */
+    protected $configuration;
+    
+    /**
+     * Service manager instance.
+     * @var BedRest\ServiceManagaer
+     */
+    protected $serviceManager;
+    
+    /**
+     * Event manager instance.
+     * @var Doctrine\Common\EventManager
+     */
+    protected $eventManager;
+    
+    /**
+     * The resource metadata factory.
+     * @var BedRest\Mapping\ResourceMetadataFactory
+     */
+    protected $resourceMetadataFactory;
+    
+    /**
+     * Constructor.
+     * @param BedRest\Configuration $configuration 
+     */
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+        
+        $this->resourceMetadataFactory = new ResourceMetadataFactory();
+        $this->resourceMetadataFactory->setClassMetadataFactory($this->configuration->getEntityManager()->getMetadataFactory());
+        $this->resourceMetadataFactory->setMetadataDriver($configuration->getMetadataDriverImpl());
+    }
+    
+    /**
+     * Returns the configuration object.
+     * @return BedRest\Configuration
+     */
+    public function getConfiguration()
+    {
+        return $this->configuration;
+    }
+    
+    /**
+     * Returns resource metadata for a class.
+     * @param string $className
+     * @return BedRest\Mapping\ResourceMetadata
+     */
+    public function getResourceMetadata($className)
+    {
+        return $this->resourceMetadataFactory->getMetadataFor($className);
+    }
+    
+    /**
+     * Returns the resource metadata factory.
+     * @return BedRest\Mapping\ResourceMetadataFactory
+     */
+    public function getResourceMetadataFactory()
+    {
+        return $this->resourceMetadataFactory;
+    }
+    
+    /**
+     * Sets the service manager.
+     * @param BedRest\ServiceManager $serviceManager 
+     */
+    public function setServiceManager(ServiceManager $serviceManager)
+    {
+        $this->serviceManager = $serviceManager;
+    }
+    
+    /**
+     * Returns the service manager.
+     * @return BedRest\ServiceManager
+     */
+    public function getServiceManager()
+    {
+        return $this->serviceManager;
+    }
+    
+    /**
+     * Sets the event manager.
+     * @param Doctrine\Common\EventManager $eventManager 
+     */
+    public function setEventManager(EventManager $eventManager) {
+        $this->eventManager = $eventManager;
+    }
+    
+    /**
+     * Returns the event manager.
+     * @return Doctrine\Common\EventManager 
+     */
+    public function getEventManager()
+    {
+        return $this->eventManager;
+    }
+    
+    /**
+     * Dispatches a request to a service to be handled and then returns the result in a Response object.
+     * @param Request $request
+     * @param Response $response 
+     */
+    public function dispatch(Request $request)
+    {
+        $rm = $this->getResourceMetadata($request->getResource());
+        
+        $sm = $this->getServiceManager();
+        $service = $sm->getService($rm->getServiceClass());
+        
+        // determine event
+        $method = $rm->getServiceMethod($request->getMethod());
+        
+        // TODO: create event
+        
+        // TODO: dispatch event
+        //$this->getEventManager()->dispatchEvent($eventName, $eventArgs);
+    }
+}
---
+++ library/BedRest/ServiceManager.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+namespace BedRest;
+
+use BedRest\Configuration;
+use Doctrine\ORM\EntityManager;
+
+/**
+ * ServiceManager
+ * 
+ * Service manager for controlling instances of various services in use by
+ * the system.
+ *
+ * @author Geoff Adams <geoff@dianode.net>
+ */
+class ServiceManager
+{
+    /**
+     * BedRest configuration.
+     * @var BedRest\Configuration
+     */
+    protected $configuration;
+    
+    /**
+     * Doctrine entity manager to be provided to services.
+     * @var Doctrine\ORM\EntityManager
+     */
+    protected $entityManager;
+    
+    /**
+     * Constructor.
+     * @param BedRest\Configuration $configuration 
+     */
+    public function __construct(Configuration $configuration)
+    {
+        $this->configuration = $configuration;
+        
+        $this->entityManager = $configuration->getEntityManager();
+    }
+    
+    /**
+     * Returns an instance of the specified service.
+     * @param string $serviceClass
+     */
+    public function getService($serviceClass)
+    {
+        // TODO: implement
+    }
+}
